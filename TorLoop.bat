0bc748d37a178a53c60e64967651afd5e4161006
Lee, Derek <Dereklee9ab@aol.com>
2017-09-25 18:59:20 -0400

    Update TorLoop.bat

--- TorLoop.bat
+++ TorLoop.bat
@@ -1,6 +1,7 @@
 :loop
-start Tor\tor.exe
-timeout /t 5
-node bot.js baseDomain pollId choiceId
+start /min Tor\tor.exe
+SET /A rtime=%RANDOM%*6/32768+6
+timeout %rtime%
+node bot.js
 taskkill /IM tor.exe
 goto loop
